f2b7b99496106bf5c01e7d9ff74551ccd3e0e73b
Prashant Jha <prashanttechjha@gmail.com>
2024-03-31 01:37:54 +0530

    [feat][design] bootstrap

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 require("dotenv").config();
 const express = require("express");
+const { resolve, dirname } = require("node:path");
 const { setupRoutes } = require("./routes");
 const { runSeeding } = require("./db/seeding");
+const cors = require("cors")
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+const VIEWS_DIR = resolve(__dirname, "./views");
+const ASSETS_DIR = resolve(__dirname, "./assets");
+
+app.set("view engine", "pug");
+app.set("views", VIEWS_DIR);
+
+app.use("/gametheory/static", express.static(ASSETS_DIR));
+
+app.use(cors());
 app.use(express.text());
 app.use(express.json());
 
--- routes/game.js
+++ routes/game.js
@@ -1,8 +1,19 @@
-exports.getGame = async () => {
-    try {
+const { pg_client } = require("../db");
+const { knex } = require("../db/query-builder");
 
-    }catch(err){
-        resp.json({ message: "Something went wrong", status: 500 }).status(500);
+exports.getGame = async (req, resp) => {
+  try {
+    let data = null;
+    const query = knex("game").select("*").toString();
+    const result = await pg_client.query(query);
 
-    }
-}
+    data = {
+      ...(result.rows[0] || {}),
+    };
+    console.log(data);
+
+    resp.render("game", data);
+  } catch (err) {
+    resp.json({ message: "Something went wrong", status: 500 }).status(500);
+  }
+};
