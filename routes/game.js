f11eeba8a7f8e3c0a482081543412e7267b293bd
Prashant Jha <prashanttechjha@gmail.com>
2024-04-01 13:07:14 +0530

    [feat][design] aded desgin

--- routes/game.js
+++ routes/game.js
@@ -4,17 +4,42 @@ const { knex } = require("../db/query-builder");
 exports.getGame = async (req, resp) => {
   try {
     let data = null;
-    const query = knex("game").select("*").toString();
-    const result = await pg_client.query(query);
+    let roomCode = null;
+    let personId = null;
+
+    personId = req.session.authentication.person_id;
+    roomCode = req.query.room;
 
     const query2 = knex("opinions").select("*").toString();
-    const opnionsRes = await pg_client.query(query2);
+    const opnionsQueryResult = await pg_client.query(query2);
+
+    const query = knex("rooms")
+      .select(
+        "rooms.code",
+        "opinions.description as opinion",
+        "rooms.player_one",
+        "rooms.player_two",
+        "game.grid",
+        "game.grid_length"
+      )
+      .where("player_one", personId)
+      .where("room_code", roomCode)
+      .orWhere("player_two", personId)
+      .innerJoin("game", "game.room_code", "rooms.code")
+      .innerJoin("opinions", "opinions.opinion_id", "rooms.opinion_id")
+      .toString();
+
+    const gameQueryResult = await pg_client.query(query);
 
     data = {
-      ...(result.rows[0] || {}),
-      opinions: opnionsRes.rows,
+      ...(gameQueryResult.rows[0] || {}),
+      opinions: opnionsQueryResult.rows,
+      person_id: personId
     };
+
+    console.log(data)
     resp.render("game", data);
+
   } catch (err) {
     console.log(err);
     resp.json({ message: "Something went wrong", status: 500 }).status(500);
--- session-handler.js
+++ session-handler.js
@@ -1,9 +1,12 @@
 const { v4: uuidv4 } = require("uuid");
 
 async function sessionHandler(req, resp, next) {
-  req.session.authentication = {}
-  req.session.authentication.person_id = uuidv4();
-  next()
+  if (!req.session?.authentication?.is_logged_in) {
+    req.session.authentication = {};
+    req.session.authentication.person_id = uuidv4();
+    req.session.authentication.is_logged_in = true;
+  }
+  next();
 }
 
 module.exports = { sessionHandler };
